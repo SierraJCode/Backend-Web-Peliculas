ecdfea6c5722ddf7cf8312a72e2eba36e850cd8c
Jesus Sierra <sierraja06@gmail.com>
2024-07-03 03:50:08 -0500

    Conectando el modulo con el Schema

--- src/movies/movies.module.ts
+++ src/movies/movies.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { MoviesService } from './movies.service';
 import { MoviesController } from './movies.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Movie, MovieSchema } from './movie.schema';
 
 @Module({
+  imports: [MongooseModule.forFeature([{name: Movie.name, schema: MovieSchema}])],
   providers: [MoviesService],
   controllers: [MoviesController]
 })
