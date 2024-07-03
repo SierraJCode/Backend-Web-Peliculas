6b932d640a06bba36f48c9c2cc20b1a344398a6d
Jesus Sierra <sierraja06@gmail.com>
2024-07-03 03:58:29 -0500

    Constructor de Service

--- src/movies/movies.service.ts
+++ src/movies/movies.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Movie } from './movie.schema';
+//import { CreateMovieDTO } from './movie.dto';
+import { Model } from 'mongoose';
 
 @Injectable()
-export class MoviesService {}
+export class MoviesService {
+
+    constructor(@InjectModel(Movie.name) private movieModel: Model<Movie>){}
+
+
+}
