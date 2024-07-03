f98349ca10b1e642cbfb80ccac5e40e02a645da4
Jesus Sierra <sierraja06@gmail.com>
2024-07-03 03:33:19 -0500

    Conectando con MongoDB

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { MoviesModule } from './movies/movies.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('monggose://localhost/moviesDB')],
+  imports: [MongooseModule.forRoot('monggose://localhost/moviesDB'), MoviesModule],
   controllers: [AppController],
   providers: [AppService],
 })
