c4956c948ae9e14e4fbc9ffb113bf82dec443c53
Jesus Sierra <sierraja06@gmail.com>
2024-07-06 04:50:40 -0500

    CORS en el backend para los permisos

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  })
   await app.listen(3000);
 }
 bootstrap();
